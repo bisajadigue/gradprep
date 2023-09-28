848973d10f5f66900a563057cfe84ad37248fa23
Veivel <25278964+Veivel@users.noreply.github.com>
2023-09-28 19:26:27 +0700

    edit readme and configurations, add PlusJakarta font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6d00f9',
+        secondary: '#eae7fb'
+      },
+      fontFamily: {
+        jakarta: 'jakarta'
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
