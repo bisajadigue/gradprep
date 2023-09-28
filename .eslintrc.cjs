f3fac2bf9ff3f6169663cad562e0dcc67d7eb5df
Veivel <25278964+Veivel@users.noreply.github.com>
2023-09-28 23:24:45 +0700

    feat: add package, edit eslint rules, styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,10 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
