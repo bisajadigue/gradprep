4b1761c80db97d1cc7285c094a07da9ad47e1dd8
risalstr <risa.lestari2002@gmail.com>
2023-09-30 23:40:54 +0700

    update program

--- src/components/modules/ProgramModule/index.tsx
+++ src/components/modules/ProgramModule/index.tsx
@@ -10,6 +10,10 @@ export const ProgramModule: React.FC = () => {
       <div className="relative flex h-[74vh] flex-col items-center justify-center bg-primary text-white">
         <div className="container flex flex-row">
           <div className="flex max-w-[60%] flex-col justify-center">
+            <div className=" absolute -left-32 -top-48 z-0 h-[80vh] w-[80vh] rounded-full bg-[#5056ED]/[.42]"></div>
+            <div className=" absolute -left-40 top-64 z-0 h-[50vh] w-[50vh] rounded-full bg-[#111692]/[.42]"></div>
+            <div className=" absolute -right-48 bottom-12 z-0 h-[40vh] w-[40vh] rounded-full bg-[#111692]/[.42]"></div>
+            <div className=" absolute -bottom-56 -right-24 z-0 h-[64vh] w-[64vh] rounded-full bg-[#946CE8]/[.62]"></div>
             <h2>Temukan ratusan info beasiswa terlengkap dan terbaru!</h2>
             <p>
               Temukan ratusan info beasiswa terlengkap & terbaru mulai dari
@@ -20,7 +24,7 @@ export const ProgramModule: React.FC = () => {
         </div>
       </div>
 
-      <div className="px-20">
+      <div className=" bg-white px-20">
         <div className="my-10">
           <SearchInput className="w-1/2" />
         </div>
