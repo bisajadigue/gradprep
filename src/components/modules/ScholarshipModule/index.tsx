7dc60ea0779d4608eef14f3af38b0cfce50f68f3
risalstr <risa.lestari2002@gmail.com>
2023-09-30 20:52:45 +0700

    test

---
+++ src/components/modules/ScholarshipModule/index.tsx
@@ -0,0 +1,3 @@
+export const ScholarshipModule: React.FC = () => {
+  return <></>;
+};
---
+++ src/pages/scholarship/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ScholarshipModule } from "@/components/modules/ScholarshipModule";
+import type { NextPage } from "next";
+
+const Forum: NextPage = () => <ScholarshipModule />;
+
+export default Forum;
