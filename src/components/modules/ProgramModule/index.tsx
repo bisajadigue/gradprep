16bfdd7ec54b56cfa6e8b2f29b9a3ca1e84e9a04
risalstr <risa.lestari2002@gmail.com>
2023-09-30 23:11:10 +0700

    feat:program page

--- src/components/modules/ProgramModule/index.tsx
+++ src/components/modules/ProgramModule/index.tsx
@@ -1,3 +1,39 @@
+import Lottie from "lottie-react";
+import Programs from "../../../../public/assets/lottie/program.json";
+import { ProgramCard } from "@/components/elements/Cards/ProgramCard";
+import { SearchInput } from "@/components/elements";
+
 export const ProgramModule: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      {/* Hero */}
+      <div className="relative flex h-[74vh] flex-col items-center justify-center bg-primary text-white">
+        <div className="container flex flex-row">
+          <div className="flex max-w-[60%] flex-col justify-center">
+            <h2>Temukan ratusan info beasiswa terlengkap dan terbaru!</h2>
+            <p>
+              Temukan ratusan info beasiswa terlengkap & terbaru mulai dari
+              beasiswa partial hingga fully funded di dalam dan luat negeri.
+            </p>
+          </div>
+          <Lottie className="-mb-60" animationData={Programs} />
+        </div>
+      </div>
+
+      <div className="px-20">
+        <div className="my-10">
+          <SearchInput className="w-1/2" />
+        </div>
+        <ProgramCard
+          id={0}
+          title={""}
+          type={""}
+          funding={""}
+          providerName={""}
+          description={""}
+        />
+        {/* <ProgramCard id={0} title={"aaa"} type={"aaa"} funding={"aaa"} providerName={""} description={""} startTime="" endTime="" /> */}
+      </div>
+    </>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ export default {
       colors: {
         primary: "#6d00f9",
         secondary: "#eae7fb",
+        purple: "#6E00FA",
+        lightpurple: "#F5E8FF",
+        green: "#66D63F",
+
       },
       fontFamily: {
         jakarta: "jakarta",
